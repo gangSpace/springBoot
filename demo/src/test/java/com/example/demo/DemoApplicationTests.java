a8efc9a504c1d243e26fc939d48aa0dc3ac56796
gang1 <test@163.com>
2020-01-01 15:06:20 +0800

    123

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-131312313123+131231
+
 @SpringBootTest
 class DemoApplicationTests {
  	
 	@Test
 	void contextLoads() {
 		
+
 	}
 
 }
